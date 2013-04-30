98eddb5bf7e88d2163e2c04642b6c613230ad77b
James Halsall <james.t.halsall@googlemail.com>
2013-04-30 21:10:14 +0100

    Renaming context of build event

--- src/Tickit/CoreBundle/Navigation/Event/BuildEvent.php
+++ src/Tickit/CoreBundle/Navigation/Event/BuildEvent.php
@@ -7,13 +7,20 @@ use Symfony\Component\EventDispatcher\Event;
 /**
  * Before navigation build event.
  *
- * Event that is fired before a navigation component is built.
+ * Event that is fired when a navigation component is being built.
  *
  * @package Tickit\CoreBundle\Navigation\Event
  * @author  James Halsall <james.t.halsall@googlemail.com>
  */
-class BeforeBuildEvent extends Event
+class BuildEvent extends Event
 {
+    /**
+     * The feature item (if any)
+     *
+     * @var array
+     */
+    protected $featuredItem;
+
     /**
      * Navigation items.
      *
@@ -31,6 +38,22 @@ class BeforeBuildEvent extends Event
      * @return void
      */
     public function addItem($name, $url, $order)
+    {
+        //todo
+    }
+
+    /**
+     * Sets the featured item on the navigation.
+     *
+     * NOTE: Not all navigation components support featured items. In that case the featured item will
+     * be included amongst the regular items.
+     *
+     * @param string $name The name of the item (this is what will be output in the navigation view)
+     * @param string $url  The URL for the item (this can be relative or absolute)
+     *
+     * @return void
+     */
+    public function addFeaturedItem($name, $url)
     {
         //todo
     }
